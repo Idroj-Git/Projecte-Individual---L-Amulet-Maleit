8ec8741b908e0754ba171de7048d68cd53e35c9a
DESKTOP-FC7OJL3\eudal <a23jorsegmau@inspedralbes.cat>
2024-10-24 13:44:49 +0200

    gitignore update per fmod

--- Projecte Individual L'amulet Maleit/Assets/Scripts/ButtonController.cs
+++ Projecte Individual L'amulet Maleit/Assets/Scripts/ButtonController.cs
@@ -8,6 +8,7 @@ public class ButtonController : MonoBehaviour
     private bool isGamePaused;
     private int[] pausableScenes;
     [SerializeField] GameObject pauseMenuCanvas;
+    [SerializeField] CanvasGroup pauseMenuCanvasGroup;
     private void Start()
     {
         pausableScenes = new int[] { 1, 2 };
@@ -26,6 +27,7 @@ public class ButtonController : MonoBehaviour
 
     public void RestartGame() // tot el joc
     {
+        ClosePauseMenu();
         SceneController.LoadScene(0);
     }
 
@@ -34,23 +36,30 @@ public class ButtonController : MonoBehaviour
         SceneController.LoadScene(1);
     }
 
-    public void ExitGame()
+    public void ExitGame() //apagar el joc
     {
-        ClosePauseMenu();
         SceneController.ExitGame();
     }
+    public void OpenConfig()
+    {
+        Debug.Log("Se ha abierto la configuración, pero aun no hay nada que configurar!");
+    }
 
+
+    //*******************      PAUSE MENU    **********************//
     private void OpenPauseMenu()
     {
         isGamePaused = true;
         pauseMenuCanvas.SetActive(isGamePaused);
+        pauseMenuCanvasGroup.alpha = 0f; // Posar en el inspector (prefab) el canvasGroup.alpha = 0, quan ho tingui acabat
+        FadeIn();
         Time.timeScale = 0.0f;
     }
 
     private void ClosePauseMenu()
     {
         isGamePaused = false;
-        pauseMenuCanvas.SetActive(isGamePaused);
+        FadeOut();
         Time.timeScale = 1.0f;
     }
 
@@ -69,8 +78,30 @@ public class ButtonController : MonoBehaviour
         }
     }
 
-    public void OpenConfig()
+    private void FadeIn()
     {
-        Debug.Log("Se ha abierto la configuración, pero aun no hay nada que configurar!");
+        StartCoroutine(FadeCanvasGroup(pauseMenuCanvasGroup, pauseMenuCanvasGroup.alpha, 1f, 0.5f));
+    }
+    
+    private void FadeOut()
+    {
+        StartCoroutine(FadeCanvasGroup(pauseMenuCanvasGroup, pauseMenuCanvasGroup.alpha, 0f, 0.5f));
+    }
+
+    private IEnumerator FadeCanvasGroup(CanvasGroup cg, float start, float end, float duration)
+    {
+        float elapsedTime = 0.0f;
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.unscaledDeltaTime;
+            cg.alpha = Mathf.Lerp(start, end, elapsedTime/duration);
+            yield return null;
+        }
+        cg.alpha = end;
+
+        if (end == 0f)
+        {
+            pauseMenuCanvas.SetActive(isGamePaused);
+        }
     }
 }
